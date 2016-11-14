15dd200711902ae7a36d783a24bbb19e374a0974
Matthew Scofield <matthewgscofield@outlook.com>
2016-11-14 11:28:22 -0600

    Hello World

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Person p = new Person();
+        p.name = "Alice";
+        p.age = 30;
+        p.isAlive = true;
+        
+    }
+}
---
+++ src/com/company/Person.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by scofieldservices on 11/14/16.
+ */
+public class Person {
+    String name;
+    int age;
+    boolean isAlive;
+
+}
